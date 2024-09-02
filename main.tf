f961f5cd756e408022891da9c614b335d3b538a0
Kiran121245 <giddalurikiransai1@gmail.com>
2024-09-02 16:38:42 +0530

    Test - Adding new service

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 resource "aws_iam_role" "my_role" {
-  name = "my_role"
+  name = "my_role_codebuild"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -28,7 +28,7 @@ resource "aws_iam_role" "my_role" {
 }
 
 resource "aws_iam_policy" "my_policy" {
-  name = "my_policy"
+  name = "my_policy_codebuild"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -45,5 +45,5 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "25992-s3bucket"
+  bucket = "25992-s3bucket-codebuild"
 }
