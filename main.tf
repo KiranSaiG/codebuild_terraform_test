00384046d69208f43ec34e0dd502d829be858452
Kiran121245 <giddalurikiransai1@gmail.com>
2024-09-02 16:47:45 +0530

    Addind new service

--- main.tf
+++ main.tf
@@ -48,6 +48,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "25992-s3bucket-codebuild"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "my_bucket_2" {
   bucket = "25992-s3bucket-codebuild-2"
 }
